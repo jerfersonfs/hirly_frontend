c0d47b85af65294461f54114c01265dde15aaee6
Jerferson Freitas <jeefreitas315@gmail.com>
2026-07-02 06:12:23 -0300

    refact: Simplifying some code lines

---
+++ candidato/js/mock.js
@@ -0,0 +1,71 @@
+const candidateData = {
+  user: {
+    id: 1,
+    name: "Marcelo",
+    avatar: "images/user/Usuariofoto.png"
+  },
+
+  jobs: [
+
+    {
+      id: 1,
+      title: "Product Designer",
+      company: "Nubank",
+      location: "Remoto",
+      salary: "R$ 2300 - 2800",
+      logo: "images/logo/others/nubank.png"
+    },
+
+    {
+      id: 2,
+      title: "Software Engineer (Backend)",
+      company: "Itaú",
+      location: "Híbrido",
+      salary: "R$ 4000 - 4300",
+      logo: "images/logo/others/itau.png"
+    },
+
+    {
+      id: 3,
+      title: "Analista de Dados",
+      company: "Samsung",
+      location: "Presencial",
+      salary: "R$ 5000 - 5400",
+      logo: "images/logo/others/samsung.png"
+    },
+
+    {
+      id: 4,
+      title: "Analista de Testes",
+      company: "Ifood",
+      location: "Presencial",
+      salary: "R$ 2300 - 2500",
+      logo: "images/logo/others/ifood.png"
+    },
+
+    {
+      id: 5,
+      title: "Desenvolvedor Front-End Júnior",
+      company: "C6 Bank",
+      location: "Remoto",
+      salary: "R$ 3500 - 3700",
+      logo: "images/logo/others/c6.png"
+    }
+
+  ],
+
+    applications: [
+        {
+            jobId: 1,
+            status: "Em análise"
+        }
+    ],
+
+    insights: {
+        candidaturas: 2,
+        feedbacks: 1,
+        processos: 2,
+        vagasSalvas: 6,
+        tempoResposta: 4
+    }
+};
